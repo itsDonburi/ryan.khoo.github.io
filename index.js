1c04ead44505b0df6cb891964e3a4f4b0ee585a3
itsDonburi <kingryan840@gmail.com>
2025-05-12 23:22:44 +0800

    Fix profile picture

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 `use strict`;
 
 (function(){
-    $('nav.uk-navbar-container li > a').on('click', function(event){
+    $('nav.uk-navbar-container .uk-navbar-nav li > a, nav.uk-navbar-container .uk-navbar-dropdown li > a').on('click', function(event){
+        // let isDropbar = $(this).closest('.uk-navbar-dropdown-nav').length == 1
+        // if (isDropbar){
+        //     let isCurrentTabActive = $(this).parent().hasClass('uk-active');
+        
+        //     if (isCurrentTabActive){
+        //         return;
+        //     }
+        
+        //     $('nav.uk-navbar-container .uk-navbar-dropdown li.uk-active').removeClass('uk-active');
+        //     $(this).parent().addClass('uk-active');
+        // }
+
         // check if current event already contain uk-active, if exist then return, else remove current active and add to this event
         let isCurrentTabActive = $(this).parent().hasClass('uk-active');
     
@@ -9,7 +21,7 @@
             return;
         }
     
-        $('nav.uk-navbar-container li.uk-active').removeClass('uk-active');
+        $('nav.uk-navbar-container .uk-navbar-nav li.uk-active').removeClass('uk-active');
         $(this).parent().addClass('uk-active');
     });
 }())
